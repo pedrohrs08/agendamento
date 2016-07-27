d1400a6d9a79335eb9b754ad0381bfb3fb4bd049
Pedro Resende <pedroresende@MacBook-Pro-de-Pedro.local>
2016-07-26 23:38:23 -0300

    adding more stuff

--- app/assets/javascripts/donor.js
+++ app/assets/javascripts/donor.js
@@ -1,13 +1,13 @@
 var app = angular.module("SchedulingModule", ["ngResource"])
 
-app.factory("Donor", ['$resource',function($resource){
+app.factory("Donor", ["$resource",function($resource){
 	return $resource("/donors/:id", {id: '@id'}, {
 		index:   { method: 'GET', isArray: true, responseType: 'json' },
     	update:  { method: 'PUT', responseType: 'json' }
 	});
 }]);
 
-app.controller("DonorsController", ['$scope','Donor',function($scope, Donor) {
+app.controller("DonorsController", ["$scope","Donor",function($scope, Donor) {
 	$scope.addDonor = function(){
 		donor = Donor.save($scope.newDonor)
 	}
